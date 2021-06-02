c821f1acc38ec7b3da881d1dc1f96ad9c970a1fb
Hidalgo Ch Ponce <hidalgoponce@icloud.com>
2021-06-02 14:27:21 -0500

    Update

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -85,7 +85,7 @@
         </div>
     </div>
 
-    <div class="bg-fixed bg-center h-128" style="background-image: url({{asset('images/banner-textil.png')}}); -webkit-background-size: auto 42rem; -webkit-background-size: auto 42rem"></div>
+    <div class="bg-fixed bg-center h-128" style="background-image: url({{asset('images/banner-textil.png')}});"></div>
 
 
     <div class="md:py-12">
