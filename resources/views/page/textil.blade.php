80cf942855cef25ae88d099fdb46f1976b508bfc
tania.vanessa <tania.vanessa609@gmail.com>
2021-06-08 12:07:19 -0500

    formulario

--- resources/views/page/textil.blade.php
+++ resources/views/page/textil.blade.php
@@ -120,7 +120,7 @@
                             <!--Title-->
                             <div class="flex justify-between items-center pb-3">
                                 <p class="text-2xl font-bold">Contizar {{$item[0]}}</p>
-                                <div class="modal-close cursor-pointer z-50">
+                            <div id="modal_close{{$loop->index}}" class="modal-close cursor-pointer z-50">
                                     <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
                                         <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
                                     </svg>
@@ -171,7 +171,14 @@
                 for (var i = 0; i < closemodal.length; i++) {
                     toggleModal(id)
                 }
-
+                var close_modal = document.querySelectorAll('#modal_close'+id)
+                for (var i = 0; i < close_modal.length; i++) {
+                    close_modal[i].addEventListener('click', function(event){
+                        if(document.body.classList.contains('modal-active'))
+                            toggleModal(id)
+                    })
+                
+                }
                 document.onkeydown = function(evt) {
                     evt = evt || window.event
                     var isEscape = false
